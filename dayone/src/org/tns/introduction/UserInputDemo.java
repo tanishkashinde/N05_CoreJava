2b936f26e9b98e09d34365657766cd30d10d42f4
Tanishq <Tanishq@Tanishq>
2023-07-20 23:12:07 +0530

    First Commit

---
+++ dayone/src/org/tns/introduction/UserInputDemo.java
@@ -0,0 +1,32 @@
+package org.tns.introduction;
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner s = new Scanner(System.in);              // creating scanner object
+//		System.out.println("Enter the value of num : ");
+//		int num =  s.nextInt();                            // taking integer input form user
+//		System.out.println("The value num is : " + num);
+//		
+//		System.out.println("Enter the value of numf : ");
+//		float numf =  s.nextFloat();                            // taking float input form user
+//		System.out.println("The value num is : " + numf);
+		
+//		System.out.println("Enter the Character : ");
+//		char chr =  s.next().charAt(0);                            // taking character input form user
+//		System.out.println("The Character is : " + chr);
+		
+//		System.out.println("Enter the String for str : ");                 // Taking String as input
+//		String str = s.next();                                          // s.next() Takes only First Word
+//		System.out.println("The string is : " + str);
+		
+	
+//		System.out.println("Enter the String for str2 : ");
+//		String str2 = s.nextLine();                                    // s.nextLine() takes whole string
+//		System.out.println("The complete string is : " + str2);
+//		
+	}
+}
